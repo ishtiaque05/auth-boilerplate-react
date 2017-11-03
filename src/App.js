460ea4ea6b453b1df4f86f04210d21f9e5357938
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-11-04 03:17:10 +0600

    Authentication boiler plate - added guest route and user route - added validation of email and password - display inline error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Homepage from "./components/pages/Homepage";
+import LoginPage from "./components/pages/LoginPage";
+import DashboardPage from "./components/pages/DashboardPage";
+import SignupPage from "./components/pages/SignupPage";
+import UserRoute from "./components/routes/UserRoute";
+import GuestRoute from "./components/routes/GuestRoute";
+import TopNavigation from "./components/navigation/TopNavigation";
 
-const App = () => <div>Hello world</div>
-export default App;
+const App = ({ location, isAuthenticated }) => (
+  <div className="ui container">
+    {isAuthenticated && <TopNavigation />}
+    <Route 
+      location={ location } 
+      path="/" 
+      exact 
+      component={ Homepage } 
+    />
+   <GuestRoute 
+      location={ location } 
+      path="/login" 
+      exact 
+      component={ LoginPage } 
+    />
+    <GuestRoute 
+      location={ location } 
+      path="/signup" 
+      exact 
+      component={ SignupPage } 
+    />
+    <UserRoute 
+      location={ location } 
+      path="/dashboard" 
+      exact 
+      component={ DashboardPage } 
+    />
+
+  </div>
+);
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state){
+  return {
+    isAuthenticated: !!state.user.email
+  }
+}
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import decode from 'jwt-decode';
 import App from './App';
+import { userLoggedIn } from './actions/auth';
+import rootReducer from './rootReducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+if(localStorage.authJWT){
+  const payload = decode(localStorage.authJWT);
+  const user = {
+    token: localStorage.authJWT,
+    email: payload.email
+  };
+  store.dispatch(userLoggedIn(user));
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <Route component={App} />
+    </Provider>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
